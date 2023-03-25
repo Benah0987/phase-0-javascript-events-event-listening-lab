d441e425f72480b807d5cfaa4cadf820d410140a
Benah0987 <88454033+Benah0987@users.noreply.github.com>
2023-03-25 22:32:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+  
+    input.addEventListener('click', clickAlert);
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  }
+  //or i can do this
+
+  
+  //document.addEventListener('DOMContentLoaded', function() {
+    //addingEventListener();
+  //});
